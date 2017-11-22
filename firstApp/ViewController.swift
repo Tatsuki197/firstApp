8945b65b89df2269da6b9e418ca20e1352c2aa6a
tatsuki197 <tatsuki197@gmail.com>
2017-11-22 10:43:42 +0800

    起動時画像表示ok

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -10,16 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+var food = ["魚",
+           "お好み焼き",
+           "肉"]
+
+@IBOutlet weak var foodImage: UIImageView!
+
+    
+    @IBAction func reButton(_ sender: UIButton) {
+        
+    let r = Int(arc4random()) % food.count
+        print(r)
+        self.selectImg(hako:r)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let r = Int(arc4random()) % food.count
+        print(r)
+        self.selectImg(hako:r)
+        
+//
+//        // 表示する画像を設定する.
+//        var myImage = UIImage(named: "\(foodImage)")
+//
+//        // 画像をUIImageViewに設定する.
+//        myImageView.image = foodImage
+//
+      
+}
+            
+    func selectImg(hako:Int){
+        switch hako{
+        case 0:
+            foodImage.image = UIImage(named:"a_1.jpg")
+        case 1:
+            foodImage.image = UIImage(named:"a_2.jpg")
+        default: //2
+            foodImage.image = UIImage(named:"a_3.jpg")
+        }
+//
+////        let notificationCenter = NotificationCenter.default
+////        notificationCenter.addObserver(
+////            self,
+////            selector: Selector(("myImage:")),
+////            name:NSNotification.Name.UIApplicationDidFinishLaunching,
+////            object: nil)
+//
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+   
+    
+    
+    
+    
+
+
+    
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+
+
+
 
 
 }
-
